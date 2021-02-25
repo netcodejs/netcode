669d8782a864a512bb4ecde542688981c474e13a
LittleMoi <emwings@outlook.com>
2021-02-26 00:53:48 +0800

    chore: build-in packages"crc-32"

--- rollup.config.js
+++ rollup.config.js
@@ -1,10 +1,21 @@
 import typescript from "rollup-plugin-typescript2";
+import cjs from "@rollup/plugin-commonjs";
 
-export default {
-    input: "src/index.ts",
-    output: {
-        file: "dist/netcode.js",
-        format: "es",
+export default [
+    {
+        input: "src/index.ts",
+        output: {
+            file: "dist/netcode.cjs.js",
+            format: "cjs",
+        },
+        plugins: [cjs(), typescript()],
     },
-    plugins: [typescript()],
-};
+    {
+        input: "src/index.ts",
+        output: {
+            file: "dist/netcode.esm.js",
+            format: "es",
+        },
+        plugins: [cjs(), typescript()],
+    },
+];
