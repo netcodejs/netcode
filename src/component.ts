d4eff155cff042b695835db2b3e96d2f328abd66
LittleMoi <emwings@outlook.com>
2021-03-09 01:03:37 +0800

    chore: publish 0.0.4

--- src/component.ts
+++ src/component.ts
@@ -1,6 +1,6 @@
 import { Entity } from "./entity";
 import { NULL_NUM, NULL_STR } from "./macro";
-import { DataType, fixupSerable, ProtoOf } from "./misc";
+import { DataType, ProtoOf } from "./misc";
 import { str as hash } from "./lib/crc-32";
 import { MethodSchema } from "./component-rpc";
 import { IDataBufferReader, IDatabufferWriter } from "./data/serializable";
@@ -146,4 +146,62 @@ export type ComponentConstructor<T = any> = { new (): T } & {
 export interface IComponent {
     entity: Entity;
     __schema__: Readonly<Schema>;
+}
+
+export function fixupSerable<T extends Record<string, any>>(target: {
+    new (): T;
+}) {
+    target.prototype.ser = function (
+        this: SchemaClass<T>,
+        buffer: IDatabufferWriter
+    ) {
+        const schema = this.__schema__;
+        for (let i = 0, count = schema.count; i < count; i++) {
+            const type = schema.props[i].type;
+            const key = schema.props[i].propertyKey;
+            const value = this[key];
+            if (type.container === NONE_CONTAINER) {
+                switch (type.dataType) {
+                    case DataType.int:
+                    case DataType.i32:
+                        buffer.writeInt(value);
+                        break;
+                    case DataType.float:
+                    case DataType.f32:
+                        buffer.writeFloat(value);
+                        break;
+                    case DataType.double:
+                    case DataType.f64:
+                        buffer.writeDouble(value);
+                        break;
+                }
+            }
+        }
+    };
+    target.prototype.deser = function (
+        this: SchemaClass<T>,
+        buffer: IDataBufferReader
+    ) {
+        const schema = this.__schema__;
+        for (let i = 0, count = schema.count; i < count; i++) {
+            const type = schema.props[i].type;
+            const key = schema.props[i].propertyKey;
+            if (type.container === NONE_CONTAINER) {
+                switch (type.dataType) {
+                    case DataType.int:
+                    case DataType.i32:
+                        (this as any)[key] = buffer.readInt();
+                        break;
+                    case DataType.float:
+                    case DataType.f32:
+                        (this as any)[key] = buffer.readFloat();
+                        break;
+                    case DataType.double:
+                    case DataType.f64:
+                        (this as any)[key] = buffer.readDouble();
+                        break;
+                }
+            }
+        }
+    };
 }
