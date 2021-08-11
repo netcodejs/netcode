636eb9f9e4ba454a6cc18d85f4917f207d7b0c2d
moruiyi <emwings@outlook.com>
2021-08-11 12:16:20 +0000

    build: yml config

--- rollup.config.example.js
+++ rollup.config.example.js
@@ -3,6 +3,7 @@ import cjs from "@rollup/plugin-commonjs";
 import serve from "rollup-plugin-serve";
 import { terser } from "rollup-plugin-terser";
 
+const isWatchMode = !!process.env.ROLLUP_WATCH;
 /**
  * @type {import("rollup").RollupOptions}
  */
@@ -25,12 +26,16 @@ const obj = {
             tsconfigOverride: { compilerOptions: { declaration: false } },
         }),
         terser(),
+    ],
+};
+if (isWatchMode) {
+    obj.plugins.push(
         serve({
             open: true,
             contentBase: "example",
             port: 8889,
-        }),
-    ],
-};
+        })
+    );
+}
 
 export default obj;
